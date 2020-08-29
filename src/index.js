d18e2738bf901cc5f26d4740f4683498d0221e0a
Amabel <levintabel@gmail.com>
2020-08-29 19:01:46 +0700

    Component Reuse

--- src/index.js
+++ src/index.js
@@ -1,29 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 import faker from "faker";
+import CommentDetail from "./CommentDetail";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 3:00PM"
-        srcImage={faker.image.avatar()}
-        text="Hi 1"
-      />
-      <CommentDetail
-        author="Alexa"
-        timeAgo="Today at 4:00PM"
-        srcImage={faker.image.avatar()}
-        text="Hi 1"
-      />
-      <CommentDetail
-        author="Siri"
-        timeAgo="Today at 5:00PM"
-        srcImage={faker.image.avatar()}
-        text="Hi 1"
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 3:00PM"
+          srcImage={faker.image.avatar()}
+          text="Hi 1"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alexa"
+          timeAgo="Today at 4:00PM"
+          srcImage={faker.image.avatar()}
+          text="Hi 1"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Siri"
+          timeAgo="Today at 5:00PM"
+          srcImage={faker.image.avatar()}
+          text="Hi 1"
+        />
+      </ApprovalCard>
     </div>
   );
 };
